2d667eb55e586fd38f9e1ad8a4bbc8547d2c037a
Chris Griffith <chris.griffith@gmail.com>
2018-10-25 08:26:19 -0700

    Initial Commit
    Demos of the three components that rely on hammerJS.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule,
+    IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
